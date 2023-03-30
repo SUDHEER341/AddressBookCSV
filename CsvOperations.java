dff6d10c1f8e97a858e44e82cbddaac3a41b8e03
SUDHEER341 <sudheer601220@gmail.com>
2023-03-30 13:16:11 +0530

    Added : programm to perform operations on csv file

---
+++ CsvOperations.java
@@ -0,0 +1,38 @@
+package com.addressbookcsv;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+public class CsvOperations {
+    File file2 = new File("addressBookCsv");
+    public void csvWriter(List<Contacts> contacts) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        FileWriter fileWriter = new FileWriter(file2);
+
+
+        ColumnPositionMappingStrategy mappingStrategy= new ColumnPositionMappingStrategy();
+        mappingStrategy.setType(Contacts.class);
+        // Arrange column name as provided in below array.
+        String[] columns = new String[]{ "count", "f_name", "l_name", "address", "city", "state", "zip", "ph_no", "email" };
+        mappingStrategy.setColumnMapping(columns);
+        // Creating StatefulBeanToCsv object
+        StatefulBeanToCsvBuilder<Contacts> builder = new StatefulBeanToCsvBuilder<>(fileWriter);
+        StatefulBeanToCsv writer = builder.withMappingStrategy(mappingStrategy).build();
+        // Write list to StatefulBeanToCsv object
+        writer.write(contacts);
+        fileWriter.close();
+    }
+    public void csvReader() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file2);
+        scanner.useDelimiter(",");
+        while ((scanner.hasNext())) {
+            System.out.println(scanner.next());
+        }
+        scanner.close();
+    }
+}
